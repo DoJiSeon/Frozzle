f9b096f9c89172d8eb1d0d01d2f59b2ddadddbee
mogisin <sukjk789@naver.com>
2023-08-23 21:08:04 +0900

    초기 게임 시작 화면
    배경 색감 조정, 로고 배치, 씬 전환 버튼 생성, 설정 버튼 생성(구현 미기능), 인게임 플레이 씬 생성( 아직 구현 X ), 이벤트 함수 생성

---
+++ Frozzle/Assets/Scripts/Start_Scene_Manager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // StartSceneManager 클래스 사용을 위해
+
+public class Start_Scene_Manager : MonoBehaviour
+{
+    // 버튼 클릭시 호출
+    public void OnClickStart()
+    {
+        // 2_PlayScene 불러오기 (씬 전환)
+        SceneManager.LoadScene("2_PlayScene");
+    }
+}
