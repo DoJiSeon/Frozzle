edb7d8d794017dcdf344b5929118bc1fa6601ccb
mogisin <sukjk789@naver.com>
2023-08-30 22:24:14 +0900

    스테이지 1 넘어가는 기능
    스테이지 1 선택창 & 버튼별 씬 전환 기능 삽입, 스플래쉬 화면 뉴옹즈 로고 수정

---
+++ Frozzle/Assets/Scripts/Stage_Scene_Manager_2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage_Scene_Manager_2 : MonoBehaviour
+{
+    // 버튼 클릭시 호출
+    public void OnClickStart()
+    {
+        // Stage_1-2 불러오기 (씬 전환)
+        SceneManager.LoadScene("Stage_1-2");
+    }
+}
