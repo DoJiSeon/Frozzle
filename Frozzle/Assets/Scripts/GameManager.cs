30ffb88f3dda3f4d78c76c447b1a80cfde82face
mogisin <sukjk789@naver.com>
2023-09-04 17:08:59 +0900

    서브메뉴 기능
    저장 회사명 팀 이름으로 변경, 인게임 서브메뉴 창 생성, esc로 열고 닫기(계속하기), 되돌아가기 (스테이지 선택창으로), 종료하기, 저장하기( 기능구현은 실패한듯)

--- Frozzle/Assets/Scripts/GameManager.cs
+++ Frozzle/Assets/Scripts/GameManager.cs
@@ -1,16 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text talkText;
+    public GameObject scanObject;
+    public GameObject menuSet; //게임 오브젝트 사용
+    public GameObject player;
     void Start()
     {
-        
+        GameLoad();
     }
 
     void Update()
     {
-        
+        // esc 눌렀을 때, 서브메뉴 창 키고 끄기
+        if (Input.GetButtonDown("Cancel"))
+        { 
+            if (menuSet.activeSelf)
+                menuSet.SetActive(false);
+            else
+                menuSet.SetActive(true);
+
+        }
+    }
+
+    public void GameExit() // 게임 종료 함수
+    {
+        Application.Quit();
+    }
+
+    public void GameSave()
+    {
+        if (!PlayerPrefs.HasKey("player_pos"))
+            return;
+
+        PlayerPrefs.SetFloat("player_pos", player.transform.position.x);
+        PlayerPrefs.SetFloat("player_pos", player.transform.position.y);
+        PlayerPrefs.Save();
+
+        // 기믹의 진행 상태도 저장해야 함
+
+        menuSet.SetActive(false);
+    }
+    
+    public void GameLoad()
+    {
+        float x = PlayerPrefs.GetFloat("player_pos");
+        float y = PlayerPrefs.GetFloat("player_pos");
+
+        player.transform.position = new Vector3(x, y, 0);
     }
 }
---
+++ Frozzle/Assets/Scripts/UI_Manager/N_StageChoose.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class N_StageChoose : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("N_StageChoose");
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
