d70218a78af9dba1f978746a1ba913de322f9fe6
mogisin <sukjk789@naver.com>
2023-11-11 16:46:31 +0900

    오디오
    Bgm은 넣었는데, 슬라이더 작동이 안되고, 씬 전환 시 유지가 안 되며, 세팅 버튼 온클릭이 안됩니다. (뭔 다 안되냐

--- Frozzle/Assets/Scripts/UI_Manager/Start_Scene_Manager.cs
+++ Frozzle/Assets/Scripts/UI_Manager/Start_Scene_Manager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement; // StartSceneManager 클래스 사용을 위해
 
 public class Start_Scene_Manager : MonoBehaviour
 {
+    public Text talkText;
+    public GameObject scanObject;
+    public GameObject menuSet; //게임 오브젝트 사용
+    public GameObject player;
+
     // 버튼 클릭시 호출
     public void OnClickStart()
     {
@@ -15,4 +21,13 @@ public class Start_Scene_Manager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void resume()
+    {
+        if (menuSet.activeSelf)
+            menuSet.SetActive(false);
+        else
+            menuSet.SetActive(true);
+    }
+
 }
