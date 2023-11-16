fc95a5507a834ad8309684cfe26f432fe9c02758
서연 <131230078+elregansekwon@users.noreply.github.com>
2023-11-16 12:36:45 +0900

    총알 발사 코드 변경
    변경했는데 destroy문제 해결 안됐어요...ㅈㅅ

--- Frozzle/Assets/bullet.cs
+++ Frozzle/Assets/bullet.cs
@@ -1,50 +1,44 @@
 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class IceBullet : MonoBehaviour
+public class bullet : MonoBehaviour
 {
-    public float speed = 10f; // 총알 속도 변수
-    public float distance = 0.5f; // 감지할 최대 거리 나타내는 변수, 기본값 0.5f
-    public LayerMask isLayer; 
-    private Transform playerTransform;// player의 좌표 저장할 변수 설정
-
+    public float speed;
+    public float distance;
+    public LayerMask isLayer;
     void Start()
     {
-        Invoke("DestroyBullet", 2);
-
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform; // player을 찾아서 좌표 가져옴 
+        Invoke("DestroyBullet",2);//invoke("실행 할 함수명", 지연시간)
     }
 
     void Update()
     {
-        MoveBullet();
-    }
+	    RaycastHit2D ray = Physics2D.Raycast(transform.position,transform.right,distance,isLayer);
+	   
+        if(ray.collider != null)
+	{
+		if(ray.collider.tag == "water")// 조건문으로 부딪힌 물체 태그가 water일 때 로그를 찍게 함
+	{
+		Debug.Log("waterfreezing!!!");
+	}
+		DestroyBullet();
+	}
 
-    void MoveBullet()
-    {
-        Debug.Log("hello");
-        Vector3 direction = playerTransform.position - transform.position; // player가 향하고 있는 방향을 구하고 그 방향으로 불렛이 이동
-        Debug.Log(direction);
-        direction.Normalize(); // 방향 정규화 -> 모든 방향의 벡터 길이 1로 만듦 -> 방향에 따른 이동 속도 같아지기 위해서
-        float moveDirection = transform.rotation.eulerAngles.y == 0f ? 1f : -1f;
-        //Debug.Log((direction, moveDirection));
-        transform.Translate( direction * moveDirection * speed * Time.deltaTime);
+        if(transform.rotation.y == 0)
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+
+       else
+       {
+            transform.Translate(transform.right * -1 * speed * Time.deltaTime);
+       }
     }
 
     void DestroyBullet()
     {
-        Destroy(gameObject); // 총알 오브젝트를 파괴
-    } // 다른 Collider와 충돌했을 때 호출되는 메서드
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        
-        
-        if (isLayer == (isLayer | (1 << other.gameObject.layer))) // 충돌한 객체의 Layer가 isLayer에 해당하는지 확인
-        {
-            DestroyBullet();  // 총알 파괴
-        }
+        Destroy(gameObject);// Destroy(파괴할 오브젝트)
     }
 }
--- Frozzle/Assets/ice_attack.cs
+++ Frozzle/Assets/ice_attack.cs
@@ -1,35 +1,33 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ice_attack : MonoBehaviour
 {
-    public GameObject bullet;//총알 프리팹을 저장하는 GameObject 변수
-    public Transform pos;//발사 위치 지정 Transform 변수
-    public float cooltime;//발사 간 쿨타임
-    private float curtime;//현재 쿨타임
-    public GameObject newBullet;
-    public Vector3 playerDirection;
+    public GameObject bullet;
+    public Transform pos;
+    public float cooltime;
+    private float curtime;
+   
     void Start()
     {
-        
+      bullet = Instantiate(bullet, transform.position, transform.rotation);
     }
 
-    void Update()
-    {
-        if (curtime <=0)// 만약 현재 쿨타임이 0 이하일 경우
+   void Update()
+   {
+        if(curtime <= 0)// curtime이 0이하일 때만 Z키가 눌리게 하기.
         {
-            if (Input.GetKey(KeyCode.Z))//Z키를 누른 경우
-        {
-           Vector3 platerDirection =  transform.forward; // player의 현재 방향을 얻어서 그 방향으로 회전값을 만듦-> 그방향으로 총알 발사
-            Quaternion playerRotation = Quaternion.LookRotation(playerDirection); 
-
-            GameObject newBullet = Instantiate(bullet, pos.position, playerRotation); // 발사 위치(pos.position)와 방향(playerRotation)으로 총알을 발사
-
-           curtime=cooltime;//현재 쿨타임을 설정된 쿨타임 값으로 초기화
+          if(Input.GetKey(KeyCode.Z))
+            {
+                Instantiate(bullet,pos.position,transform.rotation);
+            }
+            curtime = cooltime;
         }
-        }
-        curtime-= Time.deltaTime;
-    }
-}
+        curtime -= Time.deltaTime;
+   }
+} 
+
+
 
