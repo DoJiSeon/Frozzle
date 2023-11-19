385bb4a8e8d82e1f483fbe2b024635b7a065c299
DoJiSeon <djs6620@gmail.com>
2023-11-19 21:18:06 +0900

    애니메이션 구현중
    마법 애니메이션 구현중

--- Frozzle/Assets/Scripts/CharacterMovement.cs
+++ Frozzle/Assets/Scripts/CharacterMovement.cs
@@ -18,6 +18,9 @@ public class CharacterMovement : MonoBehaviour // 캐릭터 이동 및 애니메이션에 쓰
     public SpriteRenderer rend; // 스프라이트 변수
     Vector3 mousePos, transPos, targetPos, targetPos_rot; // 타겟의 방향 담을 변수
 
+
+    public bool up_spell_check, right_spell_check, can_spell;
+
     private void Awake()
     {
         mouseInput = new MouseInput(); // MouseInput 스크립트에서 가져와 객체 생성
@@ -39,8 +42,9 @@ public class CharacterMovement : MonoBehaviour // 캐릭터 이동 및 애니메이션에 쓰
         rend = GetComponent<SpriteRenderer>(); // 현재 게임 오브젝트의 스프라이트렌더러 컴포넌트를 가져온다.
         flip_check = false; // 좌우 반전 할거냐?
         is_right = false; // 오른쪽 방향을 보고있냐?
-
-
+        up_spell_check = false;
+        right_spell_check = false;
+        can_spell = false;
 
     }
 
@@ -84,6 +88,7 @@ public class CharacterMovement : MonoBehaviour // 캐릭터 이동 및 애니메이션에 쓰
         {
             transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime); // 그 위치로 이동
             find(); // 방향 관련 애니메이션 지시 메소드
+            
         }
         else // 캐릭터의 위치가 마우스 위치와 같다면
         {
@@ -94,6 +99,11 @@ public class CharacterMovement : MonoBehaviour // 캐릭터 이동 및 애니메이션에 쓰
             {
                 rend.flipX = true; // 좌우반전해주고
                 is_move = false; // 움직이기 중단
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                //animator.SetTrigger("spell_t");
+                //spell();
             }
         }
 
@@ -188,6 +198,68 @@ public class CharacterMovement : MonoBehaviour // 캐릭터 이동 및 애니메이션에 쓰
                 animator.SetBool("walk", true);
                 animator.SetBool("right", false);
                 animator.SetBool("up", false);
+                is_right = true;
+                flip_check = false;
+            }
+        }
+    }
+
+    void spell()
+    {
+        targetPos_rot = new Vector3(destination.x, destination.y, 0); // 마우스 x,y 좌표 값을 타겟 포스 변수에 넣어주기, z값은 2d 니까 0!
+        Vector3 dir_rot = targetPos_rot - transform.position; // 마우스에서 현재 포지션 값을 빼서 위치 값 구하기, 포지션엔 위치, 방향 등 다양한 정보 담고 있어
+        dir_rot.y = 0;  // 이걸 왜했더라 x 값만 필요했나봐요~~^^
+        Vector3 player_pos = new Vector3(transform.position.x, transform.position.y, 0); // 핸재 포지션의 값을 넣어주기
+
+        if (animator.GetBool("up")==true) // 만약에 타겟의 y 값이 플레이어의 y 값보다 크다면? = 마우스 위치가 위쪽이라면?
+        {
+            if (animator.GetBool("right")== true) // 만약 마우스 위치가 오른쪽이라면?
+            {
+                rend.flipX = false; // 이미 오른쪽 애니메이터를 넣어둬서 뒤집을 필요가 없기 때문에 폴스
+
+                animator.SetBool("walk", false);
+                animator.SetBool("right", true);
+                animator.SetBool("up", true);
+                animator.SetTrigger("spell_t");
+                //animator.SetBool("spell", true);
+                is_right = true;
+                flip_check = false;
+            }
+            else if (animator.GetBool("right") == false) // 만약 위쪽이고 왼쪽쯤에 마우스 클릭이 있었떠라면?
+            {
+                rend.flipX = true; // 좌우반전 하기, 애니메이션을 오른쪽 방향만 넣어놨기 때문에 왼쪽 방향을 보려면 좌우반전 true 해야함
+                animator.SetBool("walk", false);
+                animator.SetBool("right", false);
+                animator.SetBool("up", true);
+                animator.SetTrigger("spell_t");
+                //animator.SetBool("spell", true);
+                is_right = false;
+                flip_check = true;
+            }
+        }
+        else if (animator.GetBool("up") == false) // y축이 아래에 있다면, 마우스가 아래쪽에 클릭 됐다면?
+        {
+            if (animator.GetBool("right") == true) // 오른쪽 방향이 클릭되있다면?
+            {
+
+                rend.flipX = true; // 현재 아래쪽 모션은 왼쪽 방향으로 가는거가 기본으로 되어있어서 오른쪽 방향 보려면 뒤집기 true 해야함
+                animator.SetBool("walk", false);
+                animator.SetBool("right", true);
+                animator.SetBool("up", false);
+                animator.SetTrigger("spell_t");
+                //animator.SetBool("spell", true);
+                is_right = false;
+                flip_check = true;
+            }
+            else if (animator.GetBool("right") == false) // 왼쪽 방향 클릭 되었다면?
+            {
+
+                rend.flipX = false;
+                animator.SetBool("walk", false);
+                animator.SetBool("right", false);
+                animator.SetBool("up", false);
+                animator.SetTrigger("spell_t");
+                //animator.SetBool("spell", true);
                 is_right = true;
                 flip_check = false;
             }
