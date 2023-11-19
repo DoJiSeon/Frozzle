e530480ed3439c647ab72a794177bf1c21793c99
Ozinger <chocosdj@naver.com>
2023-11-19 15:40:38 +0900

    대화 씬에 컷씬 추가
    ㅈㄱㄴ

--- Frozzle/Assets/Scripts/talkScript/memoryTalk.cs
+++ Frozzle/Assets/Scripts/talkScript/memoryTalk.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -13,6 +14,7 @@ public class memoryTalk : MonoBehaviour
     public GameObject nameTag;
     public TextMeshProUGUI nameTagInnerText;
     public Image fader;
+    public List<Image> imageList;
     public List<string> textList = new List<string>();
 
     bool autoStart = false;
@@ -35,6 +37,7 @@ public class memoryTalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isClickable);
         if (Input.GetMouseButtonDown(0) || autoStart) 
         {
             autoStart = false;
@@ -167,10 +170,12 @@ public class memoryTalk : MonoBehaviour
                 nameTagInnerText.text = "Ä®¸®¿ò";
                 seq = sentenceSequence(textList[clickCount]);
                 StartCoroutine(seq);
+                StartCoroutine(ImageFadeIn(7));
                 clickCount++;
             }
             else if (clickCount == 15 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(0));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -198,6 +203,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 18 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(1));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -207,6 +213,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 19 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(2));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -243,6 +250,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 23 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(3));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -252,6 +260,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 24 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(4));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -270,6 +279,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 26 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(5));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -279,6 +289,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 27 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(8));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -297,6 +308,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 29 && isClickable)
             {
+                StartCoroutine(ImageFadeIn(6));
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -306,6 +318,7 @@ public class memoryTalk : MonoBehaviour
             }
             else if (clickCount == 30 && isClickable)
             {
+                StartCoroutine(ImageAllFadeOut());
                 talkPanel.SetActive(true);
                 nameTag.SetActive(true);
                 nameTagInnerText.text = "Ä®¸®¿ò";
@@ -660,6 +673,22 @@ public class memoryTalk : MonoBehaviour
         isClickable = true;
     }
 
+    IEnumerator cutSceneStartSentenceSequence(string text_)
+    {
+        skip_seq = skipSequence(seq, text_);
+        StartCoroutine(skip_seq);
+        isClickable = false;
+        text.text = "";
+        foreach (char letter in text_)
+        {
+            text.text += letter;
+            yield return new WaitForSeconds(delay);
+        }
+        StopCoroutine(skip_seq);
+        StartCoroutine(fadeOut());
+        isClickable = true;
+    }
+
     IEnumerator nextSentenceSequence(string text_, string nextStage)
     {
         skip_seq = nextSkipSequence(seq, text_, nextStage);
@@ -706,6 +735,45 @@ public class memoryTalk : MonoBehaviour
             fader.color = new Color(0, 0, 0, fadeCount);
         }
         isClickable = true;
+    }
+
+    IEnumerator ImageAllFadeOut()
+    {
+        float imageFadeOutCount = 1;
+        int rgb = 255;
+        while (imageFadeOutCount > 0)
+        {
+            imageFadeOutCount -= 0.05f;
+            for (int i = 0; i < 9; i++ )
+            {
+                if (i >= 7)
+                {
+                    rgb = 0;
+                } else
+                {
+                    rgb = 255;
+                }
+                imageList[i].color = new Color(rgb, rgb, rgb, imageFadeOutCount);
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator ImageFadeIn(int index)
+    {
+        float imageFadeCount = 0;
+        int rgb = 255;
+        if (index >= 7)
+        {
+            rgb = 0;
+        }
+        isClickable = false;
+        while (imageFadeCount < 1.0f)
+        {
+            imageFadeCount += 0.025f;
+            yield return new WaitForSeconds(0.01f);
+            imageList[index].color = new Color(rgb, rgb, rgb, imageFadeCount);
+        }
     }
 
 
