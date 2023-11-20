4db8978d3192bc98746bcfaa94029e1344df2b86
Ozinger <chocosdj@naver.com>
2023-11-21 05:50:31 +0900

    스테이지 1 맵 찍음
    ㅈㄱㄴ

--- Frozzle/Assets/Scripts/Freeze.cs
+++ Frozzle/Assets/Scripts/Freeze.cs
@@ -10,6 +10,8 @@ public class Freeze : MonoBehaviour
     public float delay = 0.1f;
     public List<TileBase> waterRoads = new List<TileBase>();
     public List<TileBase> waterFalls = new List<TileBase>();
+    public List<TileBase> pond = new List<TileBase>();
+    public List<TileBase> bridge = new List<TileBase>();
     private List<Vector3Int> startInteraction = new List<Vector3Int>();
     private List<List<string>> decideInteraction = new List<List<string>>();
     private List<string> innerDecide = new List<string>();
@@ -23,28 +25,39 @@ public class Freeze : MonoBehaviour
         startInteraction.Clear();
         doInteraction.Clear();
         Debug.Log("hello");
-        startInteraction.Add(new Vector3Int(-1, -2, 0));
+        startInteraction.Add(new Vector3Int(3, -1, 0));
         innerDecide.Add("RoadRight");
         innerDecide.Add("RoadRight");
         innerDecide.Add("RoadRight");
         innerDecide.Add("RoadRight");
         innerDecide.Add("RoadRight");
+        innerDecide.Add("RoadRight");
+        innerDecide.Add("RoadRight");
+        innerDecide.Add("RoadRight");
+        innerDo.Add(new Vector3Int(4, -1, 0));
+        innerDo.Add(new Vector3Int(5, -1, 0));
+        innerDo.Add(new Vector3Int(6, -1, 0));
+        innerDo.Add(new Vector3Int(7, -1, 0));
+        innerDo.Add(new Vector3Int(8, -1, 0));
+        innerDo.Add(new Vector3Int(9, -1, 0));
+        innerDo.Add(new Vector3Int(10, -1, 0));
+        innerDo.Add(new Vector3Int(11, -1, 0));
+        Debug.Log("hello2");
+        decideInteraction.Add(new List<string>(innerDecide));
+        doInteraction.Add(new List<Vector3Int>(innerDo));
+        innerDecide.Clear();
+        innerDo.Clear();
+        startInteraction.Add(new Vector3Int(9, 2, 0));
         innerDecide.Add("FallEndRight");
         innerDecide.Add("FallMiddleRight");
         innerDecide.Add("FallStartRight");
-        innerDecide.Add("CornerRight");
-        innerDo.Add(new Vector3Int(0, -3, 0));
-        innerDo.Add(new Vector3Int(1, -3, 0));
-        innerDo.Add(new Vector3Int(2, -3, 0));
-        innerDo.Add(new Vector3Int(3, -3, 0));
-        innerDo.Add(new Vector3Int(4, -3, 0));
-        innerDo.Add(new Vector3Int(6, -2, 0));
-        innerDo.Add(new Vector3Int(7, -1, 0));
-        innerDo.Add(new Vector3Int(8, 0, 0));
-        innerDo.Add(new Vector3Int(9, 0, 0));
-        Debug.Log("hello2");
-        decideInteraction.Add(innerDecide);
-        doInteraction.Add(innerDo);
+        innerDo.Add(new Vector3Int(10, 4, 0));
+        innerDo.Add(new Vector3Int(11, 5, 0));
+        innerDo.Add(new Vector3Int(12, 6, 0));
+        decideInteraction.Add(new List<string>(innerDecide));
+        doInteraction.Add(new List<Vector3Int>(innerDo));
+        innerDecide.Clear();
+        innerDo.Clear();
     }
 
     IEnumerator freezeRoad(Vector3Int pos, int toChange)
@@ -59,16 +72,28 @@ public class Freeze : MonoBehaviour
         tilemap.SetTile(pos, waterFalls[toChange]);
     }
 
+    IEnumerator freezePond(Vector3Int pos, int toChange)
+    {
+        yield return new WaitForSeconds(delay);
+        tilemap.SetTile(pos, pond[toChange]);
+    }
+
+    IEnumerator makeRoad(Vector3Int pos, int toChange)
+    {
+        yield return new WaitForSeconds(delay);
+        tilemap.SetTile(pos, bridge[toChange]);
+    }
+
     // Update is called once per frame
     void Update()
     {
         //Vector3Int currentPos = tilemap.WorldToCell(player.transform.position);
+        Vector3Int currentPos = tilemap.WorldToCell(player.transform.position);
         if (Input.GetKeyDown(KeyCode.E) && waterRoads.Count > 0 && waterFalls.Count > 0)
         {
             delay = 0f;
             for (int i = 0; i < startInteraction.Count; i++)
             {
-                Vector3Int currentPos = tilemap.WorldToCell(player.transform.position);
                 if (startInteraction[i] == currentPos)
                 {
                     for (int j = 0; j < decideInteraction[i].Count; j++)
@@ -112,6 +137,22 @@ public class Freeze : MonoBehaviour
                         else if (decideInteraction[i][j] == "FallEndLeft")
                         {
                             StartCoroutine(freezeFall(doInteraction[i][j], 5));
+                        }
+                        else if (decideInteraction[i][j] == "PondRight")
+                        {
+                            StartCoroutine(freezePond(doInteraction[i][j], 0));
+                        }
+                        else if (decideInteraction[i][j] == "PondLeft")
+                        {
+                            StartCoroutine(freezePond(doInteraction[i][j], 1));
+                        }
+                        else if (decideInteraction[i][j] == "BridgeRight")
+                        {
+                            StartCoroutine(freezePond(doInteraction[i][j], 0));
+                        }
+                        else if (decideInteraction[i][j] == "BridgeLeft")
+                        {
+                            StartCoroutine(freezePond(doInteraction[i][j], 1));
                         }
                         delay += 0.1f;
                     }
