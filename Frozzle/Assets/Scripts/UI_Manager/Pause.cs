08150becba860b7b4ff74fd26f5be60f726a4960
mogisin <sukjk789@naver.com>
2023-11-21 23:35:11 +0900

    버튼 기능 수정

--- Frozzle/Assets/Scripts/UI_Manager/Pause.cs
+++ Frozzle/Assets/Scripts/UI_Manager/Pause.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
     private bool isPaused = false;
+    private bool isKeyDown = false;
     public AudioSource audioSource;
+    public Button button;
 
     void Start()
     {
+        button.onClick.AddListener(taskOnClick);
+    }
 
+    void taskOnClick()
+    {
+        isKeyDown = true;
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.Escape) || isKeyDown)
         {
+            isKeyDown = false;
             TogglePause();
             StopMusic();
         }
@@ -51,7 +60,8 @@ public class Pause : MonoBehaviour
         {
             audioSource.Stop();
         }
-        else {
+        else
+        {
             audioSource.Play();
         }
     }
